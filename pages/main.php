68b400f6f7e14da54f051a5e2983a832b6d312c3
Laszlo Bozsik <laszlo.bozsik@gmail.com>
2013-04-07 04:34:55 +0200

    Simple test code of saving and loading

--- pages/main.php
+++ pages/main.php
@@ -5,6 +5,10 @@ include_once('includes/Deck.class.php');
 
 $game = new Game();
 $game->start();
+krumo($game);
 $game->saveToDb();
+$game2 = new Game($game->id);
+krumo($game2);
+$game2->saveToDb();
 
 Template::$content = Template::renderTemplate('main', array('game' => $game));
