afa4e974e049bd968f32c35e6b28a97fcb48d021
Laszlo Bozsik <laszlo.bozsik@gmail.com>
2013-04-15 21:51:52 +0200

    Simple existing game load ability

--- pages/main.php
+++ pages/main.php
@@ -1,27 +1,29 @@
 <?php
 check_login();
-
-//poor(/lazy) man's unit tests :)
-$game = new Game();
-$game->start();
-//TEST CODE, CAN BE MODIFIED, DELETED FREELY
-$game->builtpiles['Y'] = 3;
-$game->builtpiles['R'] = 2;
-$game->action('discard', 1);
-$game->action('discard', 1);
-$game->action('discard', 1);
-$game->action('discard', 1);
-$game->action('discard', 1);
-$game->action('discard', 1);
-$game->action('discard', 1);
-$game->action('discard', 1);
-$game->action('discard', 1);
-$game->action('discard', 1);
-$game->hints = 5;
-//END OF TEST CODE
-$game->saveToDb();
+if(isset($_GET['id'])) {
+  //TODO: check if user is present in this game
+  $game = new Game($_GET['id']);
+} else {
+  //poor(/lazy) man's unit tests :)
+  $game = new Game();
+  $game->start();
+  //TEST CODE, CAN BE MODIFIED, DELETED FREELY
+  $game->builtpiles['Y'] = 3;
+  $game->builtpiles['R'] = 2;
+  $game->action('discard', 1);
+  $game->action('discard', 1);
+  $game->action('discard', 1);
+  $game->action('discard', 1);
+  $game->action('discard', 1);
+  $game->action('discard', 1);
+  $game->action('discard', 1);
+  $game->action('discard', 1);
+  $game->action('discard', 1);
+  $game->hints = 5;
+  //END OF TEST CODE
+  $game->saveToDb();
+}
 $_SESSION['currentgame'] = $game->id;
-
 krumo($game);
 
 //TODO: separate own hand
