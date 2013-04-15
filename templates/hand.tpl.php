11b6b0df73504e8332647416706d774ee436d899
Gyulus <sulugy@gmail.com>
2013-04-15 21:31:55 +0200

    Imageless hack

--- templates/hand.tpl.php
+++ templates/hand.tpl.php
@@ -1,9 +1,10 @@
 <div class="playerhand" id="player<?php echo $player->id; ?>hand">
   <h2 class="element"><?php echo $player->name; ?></h2>
   <?php foreach($player->hand as $card) { 
-    //TMP hack:
+    //Imageless help hack:
     $color = substr($card, 0, 1);
+    $number = substr($card, 1, 1);
   ?>
-  <img class="cardhand <?php echo $color; ?>" alt="1" src="images/cards/<?php echo $card; ?>.jpg">
+  <img class="cardhand <?php echo $color; ?>" alt="<?php echo $number; ?>" src="images/cards/<?php echo $card; ?>.jpg">
   <?php } ?>
 </div>
