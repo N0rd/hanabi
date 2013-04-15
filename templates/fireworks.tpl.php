fa82515edba8efdbf06015feb09adc8eda57375b
Laszlo Bozsik <laszlo.bozsik@gmail.com>
2013-04-15 16:07:47 +0200

    Fix fireworks template file encoding

--- templates/fireworks.tpl.php
+++ templates/fireworks.tpl.php
@@ -1,4 +1,4 @@
-<h2 class="element">Tüzijáték</h2>
+<h2 class="element">TÃ¼zijÃ¡tÃ©k</h2>
 <?php foreach($fireworks as $color => $number) { ?>
   <img class="cardplace <?php echo $color; ?>" src="images/cards/<?php echo $color.$number; ?>.jpg" alt="<?php echo $number; ?>" />
 <?php } ?>
