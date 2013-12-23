30087bf5c1ac22339bf73900d7560d8d62d0e339
unknown <laszlo.bozsik@gmail.com>
2013-12-23 23:44:18 +0100

    Handle session timeout

--- action.php
+++ action.php
@@ -1,41 +1,41 @@
 <?php
 require('includes/core.php');
-$game = new Game($_SESSION['currentgame']);
-if(!isset($_POST['action'])) {
-  //We will return changes here without action
-} else {
-  if(!isset($_POST['param1'])) {
-    $_POST['param1'] = null;
-  }
-  if(!isset($_POST['param2'])) {
-    $_POST['param2'] = null;
-  }
-  $output = $game->action($_POST['action'], $_POST['param1'], $_POST['param2']);
-  $game->saveToDb();
-}
-
 $refresh = array();
-if(isset($output['refresh'])) {
-  $refreshlist = $output['refresh'];
-  unset($output['refresh']);
-  foreach($refreshlist as $element) {
-    if(!isset($refresh[$element])) {
-      $refresh[$element] = Template::renderElement($element, $game);
+$logs = array();
+$error = false;
+if(!isset($_SESSION['currentgame'])) {
+  $game = false;
+  $output = array('error' => 'Jelentkezz be újra!');
+} else {
+  $game = new Game($_SESSION['currentgame']);
+  if(!isset($_POST['action'])) {
+    //We will return changes here without action
+  } else {
+    if(!isset($_POST['param1'])) {
+      $_POST['param1'] = null;
+    }
+    if(!isset($_POST['param2'])) {
+      $_POST['param2'] = null;
+    }
+    $output = $game->action($_POST['action'], $_POST['param1'], $_POST['param2']);
+    $game->saveToDb();
+  }
+
+  if(isset($output['refresh'])) {
+    $refreshlist = $output['refresh'];
+    unset($output['refresh']);
+    foreach($refreshlist as $element) {
+      if(!isset($refresh[$element])) {
+        $refresh[$element] = Template::renderElement($element, $game);
+      }
+    }
+  }
+
+  if($game->newlog) {
+    foreach($game->newlog as $log) {
+      $logs[] = Template::renderLog($log, $game);
     }
   }
 }
-
-
-$logs = array();
-if($game->newlog) {
-  foreach($game->newlog as $log) {
-    $logs[] = Template::renderLog($log, $game);
-  }
-
-}
-
-$output = array('output' => $output, 'refresh' => $refresh, 'debug' => $game, 'logs' => $logs);
-
-
-
+$output = array('output' => $output, 'refresh' => $refresh, 'debug' => $game, 'logs' => $logs, 'error' => $error);
 echo json_encode($output);
